035a99b54c7d88377711cc3fc05d34ae13d71ece
Juan Carlos Alvez <jcalvez@hotmail.com>
2020-12-30 14:38:40 -0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MylistComponent } from './components/mylist/mylist.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MylistComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
